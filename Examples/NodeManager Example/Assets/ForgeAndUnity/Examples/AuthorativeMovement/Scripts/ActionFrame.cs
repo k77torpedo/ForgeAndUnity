702f72509f02027ba8fe0256efa857eaf8458364
DEV-Segaia <segaia@gmx.de>
2019-03-18 18:54:46 +0100

    Trash Commit

---
+++ Examples/NodeManager Example/Assets/ForgeAndUnity/Examples/AuthorativeMovement/Scripts/ActionFrame.cs
@@ -0,0 +1,9 @@
+﻿/// <summary>
+/// A frame of action a player has done.
+/// </summary>
+[System.Serializable]
+public struct ActionFrame {
+    //Fields
+    public byte actionId;
+    public byte[] data;
+}
