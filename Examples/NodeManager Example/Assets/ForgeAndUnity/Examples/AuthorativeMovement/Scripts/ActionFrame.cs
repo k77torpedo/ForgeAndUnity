2c2962134107cfaea6f7a74f3cc9fd38830005a2
DEV-Segaia <segaia@gmx.de>
2019-03-21 20:16:06 +0100

    The AuthorativeMovement-Example has been moved to its own repository. You can now find it here: https://github.com/k77torpedo/ForgeAuthorativeMovement I'm very sorry if this causes any inconvenience. I'm not much used to GitHub :/

--- Examples/NodeManager Example/Assets/ForgeAndUnity/Examples/AuthorativeMovement/Scripts/ActionFrame.cs
+++
@@ -1,9 +0,0 @@
-﻿/// <summary>
-/// A frame of action a player has done with a <see cref="InputListener"/>.
-/// </summary>
-[System.Serializable]
-public struct ActionFrame {
-    //Fields
-    public byte actionId;
-    public byte[] data;
-}
