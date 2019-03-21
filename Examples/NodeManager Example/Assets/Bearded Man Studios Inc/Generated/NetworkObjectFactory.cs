b30d185284012ec4b308296210d1c2aad2d1774d
DEV-Segaia <segaia@gmx.de>
2019-03-21 20:16:21 +0100

    Trash commit.

--- Examples/NodeManager Example/Assets/Bearded Man Studios Inc/Generated/NetworkObjectFactory.cs
+++ Examples/NodeManager Example/Assets/Bearded Man Studios Inc/Generated/NetworkObjectFactory.cs
@@ -35,10 +35,6 @@ namespace BeardedManStudios.Forge.Networking.Generated
 						availableCallback = true;
 						obj = new ExampleProximityPlayerNetworkObject(networker, id, frame);
 						break;
-					case InputListenerPlayerNetworkObject.IDENTITY:
-						availableCallback = true;
-						obj = new InputListenerPlayerNetworkObject(networker, id, frame);
-						break;
 					case NetworkCameraNetworkObject.IDENTITY:
 						availableCallback = true;
 						obj = new NetworkCameraNetworkObject(networker, id, frame);
